6499d5902f504643c496cfa76120beea6d1ab83b
MGusto <felixbjorseth@outlook.com>
2025-05-07 13:31:52 +0200

    Made the app 100% more threatening

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Goodbye, world!");
+Console.WriteLine("Halla! Hva heter du?");
+var input = Console.ReadLine();
+Console.WriteLine($"Jaja, tror du er morsom, {input}...?");
+Thread.Sleep(2000);
+Console.WriteLine("...");
+Thread.Sleep(2000);
+Console.WriteLine($"Kanskje du får deg en smell, du?! Kanskje jeg henter frem kjeppen!");
+Thread.Sleep(2000);
+Console.WriteLine("...");
+Thread.Sleep(2000);
+Console.WriteLine($"Haha! Jeg bare fleiper, {input}, ikke vær redd!");
